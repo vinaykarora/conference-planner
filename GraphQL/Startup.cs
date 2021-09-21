519e9edc9d4dc389c62bb46d378d979269c7a290
Vinay Arora <vinay.arora@wwt.com>
2021-09-21 12:43:31 +0530

    Configuring EF Migrations

--- GraphQL/Startup.cs
+++ GraphQL/Startup.cs
@@ -7,8 +7,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ConferencePlanner.GraphQL.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 
-namespace GraphQL
+namespace ConferencePlanner.GraphQL
 {
     public class Startup
     {
@@ -16,6 +19,7 @@ namespace GraphQL
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
