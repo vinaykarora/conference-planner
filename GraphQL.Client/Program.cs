597cfc355ddad8ffaff16b77bb20ed8251a912b9
Vinay Arora <vinay.arora@wwt.com>
2021-09-28 12:19:52 +0530

    Add a GetSession Query and register GraphQL client

--- GraphQL.Client/Program.cs
+++ GraphQL.Client/Program.cs
@@ -19,6 +19,10 @@ namespace GraphQL.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services
+                .AddConferenceClient()
+                .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://localhost:5001/graphql"));
+
             await builder.Build().RunAsync();
         }
     }
